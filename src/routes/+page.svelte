0360d701eb7a528a5ad35fd13bbce668413d70eb
JungyoKim <58296133+JungyoKim@users.noreply.github.com>
2023-05-31 22:50:04 +0900

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -78,7 +78,7 @@ if(number==""){
                 <span class="mr-1">메이커창작과</span>
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/><polyline points="15 3 21 3 21 9"/><line x1="10" y1="14" x2="21" y2="3"/></svg>
             </button>
-            <h1 class="ml-2">의 답을 제공합니다.  (파이썬 한정)</h1></div>
+            <h1 class="ml-2">의 답을 제공합니다.</h1></div>
         </div>
         </div>
 
