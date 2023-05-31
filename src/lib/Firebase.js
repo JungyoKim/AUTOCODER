ba52a20ff5d96f9ff25079c135200d324999519d
JungyoKim <58296133+JungyoKim@users.noreply.github.com>
2023-05-31 23:03:59 +0900

    Update Firebase.js

--- src/lib/Firebase.js
+++ src/lib/Firebase.js
@@ -1,14 +1,16 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPi1_JCCG0kFY6M7LLH4vzfLbtVpqTUHs",
-  authDomain: "autocoder-js.firebaseapp.com",
-  projectId: "autocoder-js",
-  storageBucket: "autocoder-js.appspot.com",
-  messagingSenderId: "896861018272",
-  appId: "1:896861018272:web:21879e925e3b28fa6d51c5",
-  measurementId: "G-SVSQ4VD6CZ"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
 };
 
 let app;
